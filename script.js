de7098c7b43f6a299b2fbb54f6bb1220f5b648e5
JAYACHANDRA8449 <Jayachandramudiraj1@gmail.com>
2025-03-01 11:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+document.querySelector('input').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedOption = colorSelect.selectedIndex;
+        colorSelect.remove(selectedOption);
+});
